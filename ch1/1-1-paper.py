0d9c487389ec5d8de493eef8b95efe75d48f07d4
Michael Bock <mykel.bock@gmail.com>
2013-09-12 18:05:59 -0700

    Add 1.1

---
+++ ch1/1-1-paper.py
@@ -0,0 +1,18 @@
+def all_unique(s):
+	letters = {} # dict to hold if letter has been seen {char : bool}
+	for c in s:
+		if c in letters:
+			return False
+		else:
+			letters[c] = True
+	return True
+
+def all_unique_no_xtra_space(s):
+	for i, c in enumerate(s):
+		for j, d in enumerate(s):
+			if i == j: # don't compare the same letter
+				continue
+			else:
+				if c == d:
+					return False
+	return True
